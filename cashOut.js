2d737195d09b9a02ad9a9b350d720db13fc442bf
Srabonti721 <somayaaktarshabonti721@gmail.com>
2026-03-25 00:23:37 +0600

    cash out function is done

---
+++ cashOut.js
@@ -0,0 +1,9 @@
+function cashOut(money) {
+    if (typeof money !== "number" || money < 0) {
+        return "invalid";
+    }
+    const total = money * 1.75 / 100;
+    return total;
+}
+const total = cashOut(300);
+console.log(total);
